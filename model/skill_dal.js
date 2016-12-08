fa42a0c61447ba7190ca12aff8aa8f286024cabb
carvalhe <carvalhe@sonoma.edu>
2016-12-07 16:06:24 -0800

    This is all the Webstorm labs through lab 12, however lab 12 didnt get finished.

---
+++ model/skill_dal.js
@@ -0,0 +1,51 @@
+var mysql   = require('mysql');
+var db  = require('./db_connection.js');
+
+/* DATABASE CONFIGURATION */
+var connection = mysql.createConnection(db.config);
+
+/*
+ create or replace view skill_view as
+ select s.*, a.street, a.zipcode from skill s
+ join address a on a.address_id = s.address_id;
+ */
+
+exports.getAll = function(callback) {
+    var query = 'SELECT * FROM skill;';
+
+    connection.query(query, function(err, result) {
+        callback(err, result);
+    });
+};
+
+exports.getById = function(skill_id, callback) {
+    var query = 'SELECT * FROM skill WHERE skill_id = ?';
+    var queryData = [skill_id];
+
+    connection.query(query, queryData, function(err, result) {
+        callback(err, result);
+    });
+};
+
+exports.insert = function(params, callback) {
+    var query = 'INSERT INTO skill (name) VALUES (?)';
+
+    // the question marks in the sql query above will be replaced by the values of the
+    // the data in queryData
+    var queryData = [params.name];
+
+    connection.query(query, queryData, function(err, result) {
+        callback(err, result);
+    });
+
+}
+
+exports.delete = function(skill_id, callback) {
+    var query = 'DELETE FROM skill WHERE skill_id = ?';
+    var queryData = [skill_id];
+
+    connection.query(query, queryData, function(err, result) {
+        callback(err, result);
+    });
+
+};
